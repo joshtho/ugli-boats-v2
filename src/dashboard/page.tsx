306d3ac5cda52a9ad935d9dcba4a86b663a407e6
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2025-06-03 21:04:49 -0500

    Add IMAGES to public

--- src/dashboard/page.tsx
+++ src/dashboard/page.tsx
@@ -20,8 +20,8 @@ export default function Page() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-          <div className="flex items-center gap-2 px-3">
+        <header className=" flex h-16 shrink-0 items-center gap-2 border-b ">
+          <div className=" flex items-center gap-2 px-3 ">
             <SidebarTrigger />
             <Separator orientation="vertical" className="mr-2 h-4" />
             <Breadcrumb>
